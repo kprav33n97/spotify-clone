11f121c7eb4b32c70e43f19efa1b43377116c4ff
kprav33n97 <kpraveenkumar2006@gmail.com>
2024-05-05 15:03:40 +0530

    Update

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 console.log("JavaScipt Loading...");
 
+let currentSong = new Audio;
+
+function convertSecondsToMinutesSeconds(seconds) {
+    var minutes = Math.floor(seconds / 60);
+    var remainingSeconds = seconds % 60;
+
+    // Add leading zero if seconds is less than 10
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;
+    }
+
+    return minutes + ":" + remainingSeconds;
+}
+
+
 async function getSongs() {
     let a = await fetch('http://127.0.0.1:5500/songs/');
     let response = await a.text();
@@ -17,12 +32,15 @@ async function getSongs() {
 }
 
 const playMusic = (track) => {
-  let audio = new Audio("/songs/" + track)
-  audio.play()
+//   let audio = new Audio("/songs/" + track)
+  currentSong.src = "/songs/" + track
+  currentSong.play()
+  play.src = "pause.svg"
+  document.querySelector(".songInfo").innerHTML = "track";
+  document.querySelector(".songTime").innerHTML = "00:00 / 00:00";
 }
 
 async function main() {
-    let currentSong;
     let songs = await getSongs();
     console.log(songs);
 
@@ -46,12 +64,32 @@ async function main() {
     Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach((e) => {
         e.addEventListener("click", element => {
             // console.log(e.getElementsByTagName("div")[0]);
-            // console.log(e.querySelector(".info").firstElementChild.innerHTML);
-            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+            console.log(e.querySelector(".songListInfo").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".songListInfo").firstElementChild.innerHTML.trim())
         })
-    }
+    })
 
-    )
+    // Attach an event listener to play, next & previous 
+
+    play.addEventListener("click", ()=> {
+        if(currentSong.paused) {
+            currentSong.play()
+            play.src = "pause.svg"
+        }
+        else {
+            currentSong.pause()
+            play.src = "play.svg"
+        }
+    })
+
+    // Listen for timeupdate event
+
+    currentSong.addEventListener("timeupdate", () => {
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songTime").innerHTML = `
+        ${convertSecondsToMinutesSeconds(currentSong.currentTime)}/${convertSecondsToMinutesSeconds(currentSong.duration)}'
+        `
+    })
 }
 
 main()
